b90c13a3f6eeb18648611373d51028bc459aa8b0
Eric Bouchut <ebouchut@gmail.com>
2013-07-13 01:56:40 +0200

    First commit

---
+++ lib/learning-ruby.rb
@@ -0,0 +1,7 @@
+require "learning-ruby/version"
+
+module Learning
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/learning-ruby/version.rb
@@ -0,0 +1,5 @@
+module Learning
+  module Ruby
+    VERSION = "0.0.1"
+  end
+end
