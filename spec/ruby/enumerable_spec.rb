947861683a26f6d9df6a0e051329bb4bd375dd55
Eric Bouchut <ebouchut@gmail.com>
2013-07-29 06:57:58 +0200

    Use digits instead of numbers in Enumerable examples

--- spec/ruby/enumerable_spec.rb
+++ spec/ruby/enumerable_spec.rb
@@ -1,31 +1,41 @@
 describe "Enumerable" do
 
-  let(:numbers) do
-    [53, 42, 10, 19]
+  let(:digits) do
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  describe "#all?" do
-    describe "when given a block" do
+
+  context "#all?" do
+
+    context "when given a block" do
       it "return true if the block returns true for all of the items" do
-        numbers.all? {|n| n.even?}.should be_false
-        numbers.all? {|n| n < 100}.should be_true
+        expect(digits.all? {|n| n.even?}).to be_false
+        expect(digits.all? {|n| n < 10}).to be_true
+      end
+    end
+
+    context "if no block is given" do
+      it "returns true if non of the items are false or nil" do
+        expect(digits.all?).to be_true
+        expect([1, 2, false].all?).to be_false
+        expect([1, 2, nil].all?).to be_false
       end
     end
   end
 
   context "#any?" do
-    context "when given a block" do
-      it "returns true if the block is true for at least one item" do
-        expect(numbers.any? {|n| n > 20}).to be_true
-      end
+    it "returns true if the block is true for at least one item" do
+      expect(digits.any? {|n| n.even?}).to be_true
     end
   end
+
 
   context "#chunk" do
     context "when given a block" do
       pending
     end
   end
+
 
   context "#collect_concat (alias #flat_map)" do
 
@@ -34,6 +44,7 @@ describe "Enumerable" do
         expect( [[1, 2], [3, 4]].collect_concat {|array| array}).to eq( [1, 2, 3, 4])
       end 
     end
+
     context "if no  block is given" do
       it "returns an enumerator" do
         enum = [[1, 2], [3, 4]].collect_concat
@@ -45,56 +56,80 @@ describe "Enumerable" do
     end
   end
 
+
   context "#count" do
-    context "if no block  nor parameter is given" do
-      it "count the numbers of items in the numeration" do
-        expect(numbers.count).to eq(4)
+
+    context "with no parameter" do
+      it "count the number of items in the enumeration" do
+        expect(digits.count).to eq(10)
       end
     end
     context "when given a parameter (n)" do
-      it "count the number of items in the numeration, for which equals to n" do
-        expect(numbers.count(53)).to eq(1)
+      it "count the number of items in the enumeration, for which equals to n" do
+        expect([1, 2, 3, 2].count(2)).to eq(2)
       end 
+
     end
     context "when given a block" do
       it "count the number of items, for which the block yields a true value" do
-        expect(numbers.count {|n| n < 20}).to eq(2)
+        expect(digits.count {|n| n > 5}).to eq(4)
       end
     end
   end
+
+
+  context "#cycle" do
+
+    context "when given a block" do
+      it "calls the block repeatedly" do
+        pending
+      end
+    end
+
+    context "when given a parameter (n)" do
+      it "calls the block repeatly n times" do
+        sum = 0 
+        digits.cycle(2) {|n| sum += n}
+        expect(sum).to eq(2 * digits.reduce(:+))
+      end 
+    end 
+  end
+
 
   context "#find (alias #detect)" do
     context "when given a block" do 
       it "returns the first item for which the block is true" do
-        expect(numbers.find {|n| n > 20}).to eq(53)
+        expect(digits.find {|n| n > 5}).to eq(6)
       end
     end 
   end
 
+
   context "#select (alias #find_all)" do
     context "when given a block" do
       it "returns the items for which the block is true" do
-        expect(numbers.select {|n| n > 20}).to eq([53, 42])
+        expect(digits.select {|n| n > 5}).to eq([6, 7, 8, 9])
       end
     end
-  end
+  end 
+
 
   context "#map (alias #collect)" do
+
     context "when given a block" do
       it "returns an array with the values of the block" do
-        expect(numbers.map {|n| n * 2 }).to eq( [106, 84, 20, 38])
+        expect(digits.map {|n| n * 2 }).to eq( [0, 2, 4, 6, 8, 10, 12, 14, 16, 18])
       end
     end
 
     context "if no block is given" do
       it "returns an Enumerator" do
-        enum = numbers.map
+        enum = digits.map
 
         expect(enum).to be_instance_of(Enumerator) 
-        expect(enum.next).to eq(53)
-        expect(enum.next).to eq(42)
-        expect(enum.next).to eq(10)
-        expect(enum.next).to eq(19)
+        10.times do |n|
+          expect(enum.next).to eq(n)
+        end
       end
     end
   end
