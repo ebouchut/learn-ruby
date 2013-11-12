244992df3f7eec1b2e895d0c6491243ded3664c4
Eric Bouchut <ebouchut@gmail.com>
2013-11-12 20:36:26 +0000

    Add example hash#fetch with default value block

--- spec/ruby/core/hash/fetch_spec.rb
+++ spec/ruby/core/hash/fetch_spec.rb
@@ -4,13 +4,25 @@ describe "Hash#spec" do
   
   
   context "when given a key" do
+
+    context "without a block" do      
+      it "raises a KeyError if the key does not exist" do      
+        expect { subject.fetch(:brother) }.to raise_error(KeyError)
+      end
     
-    it "raises a KeyError if the key does not exist" do      
-      expect { subject.fetch(:brother) }.to raise_error(KeyError)
+      it "returns the corresponding value if the key does exist" do
+        expect(subject.fetch(:first_name)).to eq("Eric")
+      end
     end
     
-    it "returns the corresponding value if the key exists" do
-      expect(subject.fetch(:first_name)).to eq("Eric")
+    context "when given a block" do
+      it "executes the block and uses it as its default value if the key does not exist" do
+        expect(subject.fetch(:brother) {"Gil"}).to eq("Gil")
+      end
+      
+      it "returns the correspoding value if the key does exist" do
+        expect(subject.fetch(:first_name) {"G.a.f.o."}).to eq("Eric")
+      end
     end
   end
 end
